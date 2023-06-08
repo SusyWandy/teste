4ec9612053626d33dadcef8026e347ce14219802
SusyWandy <136022551+SusyWandy@users.noreply.github.com>
2023-06-08 18:52:46 -0300

    Create User.ts

---
+++ src/models/User.ts
@@ -0,0 +1,15 @@
+import { Schema, connection, model, Model, ObjectId } from 'mongoose';
+import { UserType } from '../types/UserType';
+
+const schema = new Schema<UserType>({
+    name: String,
+    email: String,
+    state: String,
+    password: String
+});
+
+const modelName = 'User';
+
+export default connection && connection.models[modelName]
+    ? (connection.models[modelName] as Model<UserType>)
+    : model<UserType>(modelName, schema);
