bc9c8a81d683755ae10739e0aa54912f88a1c4b7
SusyWandy <136022551+SusyWandy@users.noreply.github.com>
2023-06-08 18:51:44 -0300

    Create Ad.ts

---
+++ src/models/Ad.ts
@@ -0,0 +1,26 @@
+import { Schema, connection, model, Model } from 'mongoose';
+import { AdType } from '../types/AdType';
+
+const schema = new Schema<AdType>({
+    idUser: String,
+    state: String,
+    category: String,
+    categoryName: String,
+    images: [{
+        url: { type: String },
+        default: { type: Boolean }
+    }],
+    dateCreated: Date,
+    title: String,
+    price: Number,
+    priceNegotiable: Boolean,
+    description: String,
+    views: Number,
+    status: String
+})
+
+const modelName = 'Ad';
+
+export default (connection && connection.models[modelName]) ?
+    connection.models[modelName] as Model<AdType> :
+    model<AdType>(modelName, schema);
